56bbdc2c37e953ef624bba9211266da8984b2e52
Khalvat-M <mmba313@gmail.com>
2019-07-10 14:08:00 +0430

    Add trlteduos keylayout

--- device.mk
+++ device.mk
@@ -22,6 +22,16 @@ $(call inherit-product-if-exists, vendor/samsung/trlteduos/trlteduos-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/trlteduos/overlay
 
+# Keylayouts
+PRODUCT_COPY_FILES += \
+    device/samsung/trlteduos/keylayout/sec_touchkey.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/sec_touchkey.kl \
+    device/samsung/trlteduos/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/gpio-keys.kl \
+    device/samsung/trlteduos/keylayout/synaptics_rmi4_i2c.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/synaptics_rmi4_i2c.kl \
+    device/samsung/trlteduos/keylayout/Vendor_04e8_Product_7021.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/Vendor_04e8_Product_7021.kl \
+    device/samsung/trlteduos/idc/Synaptics_HID_TouchPad.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/Synaptics_HID_TouchPad.idc \
+    device/samsung/trlteduos/idc/sec_e-pen.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/sec_e-pen.idc \
+    device/samsung/trlteduos/idc/sec_touchscreen.idc:$(TARGET_COPY_OUT_SYSTEM)/usr/idc/sec_touchscreen.idc
+
 # Radio/RIL
 $(call inherit-product, device/samsung/trlte-common/radio/dual/product.mk)
 
